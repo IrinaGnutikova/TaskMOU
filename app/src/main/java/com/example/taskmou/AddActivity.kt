98a4fa31c2408272a3ce2a21bbdff68cf9205320
Ирина Гнутикова <gnutikovaira@gmail.com>
2024-03-28 14:47:47 +0300

    Изменение темной темы (всегда светлая), изменение внешнего вида активити создания задачи, реализация выбора даты на странице добавления задачи

--- app/src/main/java/com/example/taskmou/AddActivity.kt
+++ app/src/main/java/com/example/taskmou/AddActivity.kt
@@ -1,11 +1,38 @@
 package com.example.taskmou
 
+import android.annotation.SuppressLint
+import android.app.DatePickerDialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.CalendarView
+import android.widget.DatePicker
+import android.widget.EditText
+import android.widget.TextView
+import androidx.core.text.set
+import java.util.Calendar
 
 class AddActivity : AppCompatActivity() {
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add)
+
+        val pickDateButton = findViewById<Button>(R.id.btnData)
+        val textview = findViewById<TextView>(R.id.textData)
+
+        pickDateButton.setOnClickListener {
+            val calendar = Calendar.getInstance()
+            val year = calendar.get(Calendar.YEAR)
+            val month = calendar.get(Calendar.MONTH)
+            val day = calendar.get(Calendar.DAY_OF_MONTH)
+
+            val datePickerDialog = DatePickerDialog(this,
+                DatePickerDialog.OnDateSetListener { view: DatePicker, year: Int, month: Int, dayOfMonth: Int ->
+                    val selectedDateString = "$dayOfMonth.${month + 1}.$year"
+                    textview.setText(selectedDateString)
+                }, year, month, day)
+            datePickerDialog.show()
+        }
     }
 }
