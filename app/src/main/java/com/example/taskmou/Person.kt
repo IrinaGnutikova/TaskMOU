55d933ebd377e670bea93e0e314de5dfb282b25e
Ирина Гнутикова <gnutikovaira@gmail.com>
2024-04-11 18:17:36 +0300

    Модульное тестирование

--- app/src/main/java/com/example/taskmou/Person.kt
+++ app/src/main/java/com/example/taskmou/Person.kt
@@ -1,6 +1,6 @@
 package com.example.taskmou
 
-data class Person(  val name: String,
+data class Person(  val name: String, // класс, необходимый для проверки входа без подключения к облачной бд
                     val email: String,
                     val pas: String
 )
--- app/src/test/java/com/example/taskmou/PersonTest.kt
+++ app/src/test/java/com/example/taskmou/PersonTest.kt
@@ -2,8 +2,7 @@ package com.example.taskmou
 
 import org.junit.Test
 import org.junit.Assert.*
-import org.junit.jupiter.api.DynamicTest
-import org.junit.jupiter.api.TestFactory
+import org.junit.jupiter.api.DisplayName
 import org.junit.runner.RunWith
 import org.junit.runners.JUnit4
 
@@ -15,10 +14,29 @@ class PersonTest{
         val listPerson = listOf(
             Person("Irina", "ira@mail.ru", "123456"),
             Person("Vlad", "vlad@mail.ru", "111111"),
-            Person("Vasilisa", "vasa@mail.ru", "654321")
+            Person("Vasilisa", "vasa@mail.ru", "123456")
         )
         val email2 = "ira@mail.ru"
         val pas2 = "123456"
+
+        listPerson.forEach {person ->
+            if (person.email == email2 && person.pas == pas2) {
+                println("Пользователь ${person.name} вошел успешно")
+            } else {
+                println("Пользователь ${person.name} не вошел")
+            }
+        }
+    }
+
+    @Test
+    fun testEnterFalse() {
+        val listPerson = listOf(
+            Person("Irina", "ira@mail.ru", "123456"),
+            Person("Vlad", "vlad@mail.ru", "111111"),
+            Person("Vasilisa", "vasa@mail.ru", "123456")
+        )
+        val email2 = "mihail@mail.ru"
+        val pas2 = "123666"
 
         listPerson.forEach {person ->
             if (person.email == email2 && person.pas == pas2) {
